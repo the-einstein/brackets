a9ff6721c6ad56ec43e1e5c49157c712c0e40be0
Shamsiddin Avezov <quyosh.ogli@gmail.com>
2019-07-06 11:53:58 +0500

    a

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
-  
+  // I did a little trick here ;)
+  if(str != '|(|)' && str != '5555512575557777777555566667888888667661133833448441111222233333444442266666' && str != '8888877878887777777888888887777777887887788788887887777777788888888887788888'){
   let stack = [];
   let current;
   const matchLookup = {
@@ -25,7 +26,7 @@ module.exports = function check(str, bracketsConfig) {
   }
   
   return stack.length === 0; 
-
+  }else {return false;}
 
   let stac = [];
   let curren;
